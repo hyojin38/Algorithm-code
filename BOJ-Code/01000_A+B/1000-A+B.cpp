c115333cad0492b2a14b73d0fcbbb2f017a733c1
LimHyojin <hyojinlim38@gmail.com>
2021-01-25 18:42:03 +0900

    20210125 | BOJ | 01000 | A+B
    Signed-off-by: LimHyojin <hyojinlim38@gmail.com>

---
+++ BOJ-Code/01000_A+B/1000-A+B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int N, M;
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	cout << N + M;
+
+
+	return 0;
+
+}
