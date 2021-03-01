bf89254a3c69d95e88d1c51d33982cbcf31c435d
LimHyojin <hyojinlim38@gmail.com>
2021-03-02 01:18:58 +0900

    20210302 | BOJ | 10172 | 개
    Signed-off-by: LimHyojin <hyojinlim38@gmail.com>

---
+++ BOJ-Code/10172-개/10172-개.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#define endl "\n";
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	freopen("Text.txt", "r", stdin);
+
+	// \t tab 8 칸 띄우기
+	// \n 개행 커서를 한줄 밑으로 이동
+	// \' '
+	// \" "
+	// \\ \역슬래시
+	cout <<"|\\_/|"<< endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+
+	return 0;
+
+
+}
