68e5edfb581e6a0faddc7bb7baea8c7befd8c18b
LimHyojin <hyojinlim38@gmail.com>
2021-05-04 22:39:38 +0900

    20210504 | BOJ | 21608 | 상어 초등학교
    Signed-off-by: LimHyojin <hyojinlim38@gmail.com>

---
+++ BOJ-Code/21608-상어초등학교/21608-상어초등학교.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+void Input() {
+
+}
+void Solution() {
+
+
+}
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("Text.txt", "r", stdin);
+
+
+	Input();
+	Solution();
+	return 0;
+
+
+
+}
