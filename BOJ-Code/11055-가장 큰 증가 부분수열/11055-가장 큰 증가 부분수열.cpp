c6316d9a9d1ed56c578933a32f424f36b9e814eb
LimHyojin <hyojinlim38@gmail.com>
2021-05-31 23:31:55 +0900

    20210531 | BOJ | 11055 | 가장 큰 증가 부분 수열
    Signed-off-by: LimHyojin <hyojinlim38@gmail.com>

---
+++ BOJ-Code/11055-가장 큰 증가 부분수열/11055-가장 큰 증가 부분수열.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#define MAX 1000+1
+
+using namespace std;
+
+int A;
+vector <int> V;
+int DP[MAX];
+int AnsDP[MAX];
+int Ans;
+void Input() {
+	cin >> A;
+	for (int i = 0; i < A; i++) {
+		int temp;
+		cin >> temp;
+		V.push_back(temp);
+		DP[i] = V[i];
+	}
+
+}
+int max(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}void Solution() {
+
+	for (int i = 1; i < A; i++) {
+		for (int j = i - 1; j >= 0; j--) {
+			if (V[i] > V[j]) {
+				DP[i] = max(DP[i], DP[j] + V[i]);
+			}
+		}
+	}
+
+	for (int i = 0; i < A; i++) {
+		//cout << DP[i]<<" ";
+		if (DP[i] > Ans) Ans = DP[i];
+	}
+
+}
+int main() {
+
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	freopen("Text.txt", "r", stdin);
+
+	Input();
+	Solution();
+	cout << Ans;
+	return 0;
+}
---
+++ BOJ-Code/11055-가장 큰 증가 부분수열/README.md
@@ -0,0 +1,13 @@
+# [BOJ] 11055-가장 큰 증가 부분수열 
+
+### 문제 바로가기
+
+>  https://www.acmicpc.net/problem/11055
+
+### 알고리즘
+
+> - [다이나믹 프로그래밍](https://www.acmicpc.net/problem/tag/25)
+
+### 문제 풀이 및 주의
+
+> #### O(N^2)  DP사용
