0350ede5354887be7ec1a6fea7939f1476c9e1c7
LimHyojin <hyojinlim38@gmail.com>
2021-06-18 17:19:06 +0900

    20210618 | PG | Level1 | 약수의 합
    Signed-off-by: LimHyojin <hyojinlim38@gmail.com>

---
+++ Programmers-Code/코딩테스트연습/Level1-약수의 합/Level1_약수의_합.java
@@ -0,0 +1,38 @@
+package Programmers;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Level1_약수의_합 {
+	
+	static int N;
+	 public static void main(String[] args) throws NumberFormatException, IOException {
+		 // Scanner sc = new Scanner(System.in);
+	     // System.out.print(Arrays.toString(solution(n)));
+		 	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			N= Integer.parseInt(br.readLine());
+		 
+			//BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			//int TC=Integer.parseInt(br.readLine());
+			
+			System.out.println(solution(N));
+	    }
+	 
+	 private static int solution(int s) {
+		 int ans=0;
+		 for(int i=1;i<=Math.sqrt(s);i++) {
+			 if(i*i >s) return ans;
+			 if(s%i==0) {
+				 int temp=s/i;
+				// System.out.println(i);
+				 ans+=i;
+				  if(i!=temp){
+	                     ans+=temp;
+	                }
+			 }
+			 
+		 }
+		return ans;
+	 }
+}
